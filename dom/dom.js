3c296228b2e682ceb61a397e1995cbc7c07a4de6
hdebesay <HDebesay@sea.yearup.org>
2022-05-30 19:33:54 -0700

    added js

---
+++ dom/dom.js
@@ -0,0 +1,30 @@
+
+// console.dir(document);
+
+// console.log(document.domain); //<--Shows IP Address
+// console.log(document.URL);
+
+// // 
+// console.log(document.title);
+// // 
+// console.log(document.doctype);
+// // 
+// console.log(document.head);
+// // 
+// console.log(document.body);
+// // 
+// console.log(document.all);
+// // 
+// console.log(document.all[13]);
+// // 
+// document.all[10].textContent = 'We Just Changed The Header';
+
+// console.log(document.forms);
+// console.log(document.links);
+// console.log(document.images); 
+
+// ////////////////////////////////////////////////////////////////////////////
+
+// Access the DOM with GETELEMENTBYID 
+
+console.log(document.getElementById('header-title'));
