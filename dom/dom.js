bb437d9eae5c56de0449ee74924cf1788ea55101
hdebesay <HDebesay@sea.yearup.org>
2022-05-31 14:07:11 -0700

    selecting with index numbers

--- dom/dom.js
+++ dom/dom.js
@@ -19,7 +19,9 @@
 // // 
 // document.all[10].textContent = 'We Just Changed The Header';
 
-// console.log(document.forms);
+// document object model can scan the tree for element
+
+// console.log(document.form);
 // console.log(document.links);
 // console.log(document.images); 
 
@@ -28,3 +30,43 @@
 // Access the DOM with GETELEMENTBYID 
 
 console.log(document.getElementById('header-title'));
+
+//creation of an object variable using the document function.
+
+let headerTitle = document.getElementById('header-title');
+
+console.log(headerTitle);
+//
+headerTitle.textContent = "I am changing the header";
+//
+headerTitle.innerHTML = "I am changing it again"
+//
+headerTitle.style.color = "hotpink";
+
+
+///////////////getElementByClassName//////////////////////////////////////////////////////////
+
+
+let items = document.getElementsByClassName('list-group-item');
+//
+console.log(items);
+//
+console.log(items[0])
+//
+items[1].textContent = "Alpha Keppa Alpha";
+items[1].style.fontWeight = "bold";
+items[1].style.backgroundColor = "yellow";
+items[1].textContent = "Alpha Keppa Alpha";
+items[1].style.color = "green";
+//
+items[0].textContent = "Delta Sigma Theta";
+items[0].style.fontWeight = "bold";
+items[0].style.backgroundColor = "blue";
+items[0].textContent = "Skee Wee";
+items[0].style.color = "white";
+//
+items[2].textContent = "Hello world number 2";
+items[2].style.fontWeight = "bold";
+items[2].style.backgroundColor = "pink";
+items[2].textContent = "Skee Wee";
+items[2].style.color = "red";
